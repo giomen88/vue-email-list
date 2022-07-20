84d3ea98a2d66683f26353f225af2310638034b3
giomen88 <giorgia9288@virgilio.it>
2022-07-20 14:38:43 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+console.log('ok', Vue);
+
+Vue.config.devtools = true;
+
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+const root = new Vue({
+    name: 'VueEmailList',
+    el: '#root',
+    data: {
+
+    },
+
+    
+
+
+})
